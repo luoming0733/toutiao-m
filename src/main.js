8332905ecae31a895112c2ff606772b840bb63bd
罗明 <luoming0733@126.com>
2021-02-20 20:18:16 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载 vant 全局样式
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import '../src/styles/index.less'
 
+// 加载图标样式
+import '../src/styles/icon.less'
 Vue.config.productionTip = false
+
+// 注册使用 vant 组件库
+Vue.use(Vant)
 
 new Vue({
   router,
